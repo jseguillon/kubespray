95d9662880d93cc97a0844269879ac551b06cb8d
Joel Seguillon <joel.seguillon@gmail.com>
2021-01-26 19:36:24 +0100

    try CI_PLATFORM export before make

--- tests/scripts/testcases_run.sh
+++ tests/scripts/testcases_run.sh
@@ -19,6 +19,7 @@ fi
 export ANSIBLE_REMOTE_USER=$SSH_USER
 export ANSIBLE_BECOME=true
 export ANSIBLE_BECOME_USER=root
+export CI_PLATFORM
 
 cd tests && make create-${CI_PLATFORM} -s ; cd -
 ansible-playbook tests/cloud_playbooks/wait-for-ssh.yml
